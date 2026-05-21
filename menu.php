8f28def80b4c7185d4e9c4a1e953df0004f180fb
MR-alyyy <aly.matheus2342@gmail.com>
2026-05-21 16:15:58 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -6,5 +6,38 @@ if (isset($_POST['logout']))
 
 <form method="post">
     <button name="logout">Sair</button>
+    <button name="add" type="inserir">cadastro santo</button>
 </form></body>
 </html>
+
+<?php
+include "conexao.php";
+
+
+
+$mensagem = "";
+
+if (isset($_POST['inserir'])) {
+
+
+$santo = trim($_POST['santo']);
+
+$erro = false;
+
+
+$stmt = $conexao->prepare("INSERT INTO menu (santo) VALUES (?)");
+
+$stmt->bind_param("s", $santo);
+
+if ($stmt->execute()) {
+    $mensagem = "<p class='sucesso'>Cadastro realizado com sucesso!</p>";
+} else {
+    $mensagem = "<p class='erro'>Erro ao cadastrar: ".$stmt->error."</p>";
+}
+
+$stmt->close();
+
+}
+
+
+?>
